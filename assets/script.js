92b470f58c31755808862e467ee6b170ed7dd1a0
albenchris <albenchris00@gmail.com>
2020-12-18 13:48:02 -0600

    added functions to generate random characters

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,29 @@
 //add window.confirms to ask types of characters allowed for the password
-  //lowercase, uppercase, numeric, and/or special characters
+  //lowercase, uppercase, numeric, and/or special characters (  !"#$%&'()*+,-./  :;<=>?@  [\]^_`  {|}~  )
 //add window.prompt to ask the desired password length
   //between 8 and 128 characters
 //data is processed and generated either in an alert or ideally on the webpage
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+console.log(getRandomLower());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+console.log(getRandomUpper());
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+console.log(getRandomNumber());
+
+function getRandomSymbol() {
+  var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+console.log(getRandomSymbol());
 
 
 // Assignment Code
