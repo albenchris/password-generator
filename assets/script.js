af089d2070844ce0a0dab070ec3ef35eae48d3a2
albenchris <albenchris00@gmail.com>
2020-12-20 11:38:00 -0600

    generatePassword function displays within textarea

--- assets/script.js
+++ assets/script.js
@@ -4,43 +4,87 @@
   //between 8 and 128 characters
 //data is processed and generated either in an alert or ideally on the webpage
 
-var howLong = window.prompt("How long would you like your password to be?") 
-while (howLong < 8 || howLong > 128) {
-  howLong = window.prompt("This generator can make passwords between 8 and 128 characters long. How long would you like your password to be?");
-}
-howLong = parseInt(howLong);
 
 
-var uppercaseConfirm = window.confirm("Can your password contain uppercase letters?");
-var lowercaseConfirm = window.confirm("Can it contain lowercase letters?");
-var numbersConfirm = window.confirm("How about numbers?");
-var symbolsConfirm = window.confirm("And symbols?");
-while (!(uppercaseConfirm || lowercaseConfirm || numbersConfirm || symbolsConfirm)) {
-  window.alert("You must select at least one character type!");
+
+
+
+function userInput() {
+  var howLong = window.prompt("How long would you like your password to be?") 
+  while (howLong < 8 || howLong > 128) {
+    howLong = window.prompt("This generator can make passwords between 8 and 128 characters long. How long would you like your password to be?");
+  }
+  
   var uppercaseConfirm = window.confirm("Can your password contain uppercase letters?");
   var lowercaseConfirm = window.confirm("Can it contain lowercase letters?");
   var numbersConfirm = window.confirm("How about numbers?");
   var symbolsConfirm = window.confirm("And symbols?");
+  console.log(uppercaseConfirm, lowercaseConfirm, numbersConfirm, symbolsConfirm)
+  while (!(uppercaseConfirm || lowercaseConfirm || numbersConfirm || symbolsConfirm)) {
+    window.alert("You must select at least one character type!");
+  }
+  var userChoices = {
+    length: howLong, 
+    upper: uppercaseConfirm,
+    lower: lowercaseConfirm,
+    number: numbersConfirm,
+    symbol: symbolsConfirm
+  }
+  console.log(userChoices);
+  return userChoices;
 };
+
+
+
+function generatePassword() {
+  var uInput = userInput();
+  var tempPassword = [];
+  if (uInput.upper) {
+    getRandomUpper();
+  }
+  if (uInput.lower) {
+    getRandomLower();
+  }
+  if (uInput.number) {
+    getRandomNumber();
+  }
+  if (uInput.symbol) {
+    getRandomSymbol();
+  }
+  var newPassword = [uInput.upper, uInput.lower, uInput.number, uInput.symbol];
+  console.log(newPassword);
+  return newPassword;
+
+};
+
+
 
 
 
 
 function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  var uppers = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  console.log(uppers);
+	return uppers;
 };
 
 function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var lowers = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  console.log(lowers);
+	return lowers;
 };
 
 function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  var numbers = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  console.log(numbers);
+  return numbers;
 };
 
 function getRandomSymbol() {
-	const symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-	return symbols[Math.floor(Math.random() * symbols.length)];
+  const symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  var randomSymbols = [Math.floor(Math.random() * symbols.length)];
+  console.log(randomSymbols);
+	return randomSymbols;
 };
 
 
